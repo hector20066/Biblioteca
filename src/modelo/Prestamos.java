039f7a3f8194bebee15dea98a49909f11e1d9a88
hector20066 <hectorluisacevedocontreras00@gmail.com>
2025-04-14 19:43:27 -0500

    Se ha implementado la logica de la clase Prestamos

--- src/modelo/Prestamos.java
+++ src/modelo/Prestamos.java
@@ -4,10 +4,36 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Ing. Hector Acevedo
  */
 public class Prestamos {
     
+    private Persona persona;
+    private Libros libros;
+    private LocalDate fechaPrestamo;
+
+    public Prestamos(){
+        
+    }
+
+    public void setFechaPrestamos(LocalDate fechaPrestamos){
+        this.fechaPrestamo = fechaPrestamos;
+    }
+
+    public LocalDate getFechaPrestamo(){
+        return fechaPrestamo;
+    }
+
+    public Persona getPersona(){
+        return persona;
+    }
+
+    public Libros getLibros(){
+        return libros;
+    }
+
 }
